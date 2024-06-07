94d5e4a1ea77252ebfb34e10fe2b3b2b1fadda23
charlesC137 <139760747+charlesC137@users.noreply.github.com>
2024-06-07 21:02:00 +0100

    Update sign-up.js

--- src/sign-up.js
+++ src/sign-up.js
@@ -10,32 +10,14 @@ const viewPass = document.querySelector(".view-pw");
 const viewImg = document.querySelector(".view-img");
 const errorMsg = document.querySelector(".error-msg");
 
-const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-const usernameRegex = /^\w+/;
-const passwordRegex = /^[@$!%*#?&a-zA-Z0-9._-]+$/;
-
-class user {
-  constructor(userLogins) {
-    this.userLogins = userLogins;
-    this.cart = [];
-  }
-}
-
-class userLogins {
-  constructor(email, username, password) {
-    this.email = email;
-    this.password = password;
-    this.username = username;
-  }
-}
-
 viewPass.addEventListener("click", (e) => {
   e.preventDefault();
   togglePassword(passwordInput, viewImg);
 });
 
 signUpBtn.addEventListener("click", (e) => {
-  
+  e.preventDefault();
+  signUserUp();
 });
 
 function togglePassword(input, img) {
@@ -48,56 +30,18 @@ function togglePassword(input, img) {
   }
 }
 
-// async function signUserUp() {
-//   let state;
+async function signUserUp() {
+  const response = await fetch('https://api-selham.onrender.com/api/signup', {
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json"
+    },
+    body:{
+      "username": `${usernameInput.value}`,
+      "email": `${emailInput.value}`,
+      "password": `${passwordInput.value}`
+    }
+  });
 
-//   if (
-//     emailRegex.test(emailInput.value) &&
-//     usernameRegex.test(username.value) &&
-//     passwordRegex.test(passwordInput.value)
-//   ) {
-//     if (users.length === 0) {
-//       state = true;
-//     } else {
-//       for (const user of users) {
-//         if (emailInput.value === user.userLogins.email) {
-//           errorMsg.textContent = "E-mail already registered";
-//           state = false;
-//           break;
-//         } else if (usernameInput.value === user.userLogins.username) {
-//           errorMsg.textContent = "Username already in use";
-//           state = false;
-//           break;
-//         } else {
-//           state = true;
-//         }
-//       }
-//     }
-
-//     if (state) {
-//       users.push(
-//         new user(
-//           new userLogins(
-//             emailInput.value,
-//             usernameInput.value,
-//             passwordInput.value
-//           )
-//         )
-//       );
-
-//       await modData(users);
-//       currentUser = new user(
-//         new userLogins(
-//           emailInput.value,
-//           usernameInput.value,
-//           passwordInput.value
-//         )
-//       );
-
-//       localStorage.setItem("selham_currentUser", JSON.stringify(currentUser));
-//       window.open("log-in.html", "_self");
-//     }
-//   } else {
-//     errorMsg.textContent = "Invalid input";
-//   }
-// }
+  console.log(response)
+}
